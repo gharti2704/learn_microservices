f7150928063eaef21c9a3acc41a93507fcb05a3f
Raju Gharti <gharti.rg@gmail.com>
2024-01-03 13:07:39 -0500

    initial setup

---
+++ src/test/java/com/learn_microservices/LearnMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
